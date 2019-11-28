0aa463704cd22cb4ec0e6e518eada000ce7523f3
schulzy@freemail.hu <schulzy@freemail.hu>
2019-11-28 22:50:32 +0100

    Initialize project

---
+++ ScheduleTaskRunner/ScheduleTaskRunner/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using ScheduleTaskRunner.Views;
+using System.Windows;
+
+namespace ScheduleTaskRunner
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
